c3e8bfb578301be559e0179d92120d88267a79d7
osboxes.org <matthias.herpers@gmail.com>
2019-05-02 21:49:23 +0200

    created IntegrationTestFile at location

---
+++ src/test/java/io/jenkins/plugins/analysis/warnings/groovy/SnippetGeneratorITest.java
@@ -0,0 +1,9 @@
+package io.jenkins.plugins.analysis.warnings.groovy;
+
+/**
+ * Tests the Snippet Generator for the pipeline syntax.
+ *
+ * @author Matthias Herpers
+ */
+class SnippetGeneratorITest {
+}
