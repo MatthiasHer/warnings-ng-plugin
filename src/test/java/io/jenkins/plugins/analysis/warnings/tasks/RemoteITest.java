91a1c19db34f2f848f3b4db9d4a40e723607c502
osboxes.org <matthias.herpers@gmail.com>
2019-05-26 10:40:40 +0200

    slavetesting

--- src/test/java/io/jenkins/plugins/analysis/warnings/tasks/RemoteITest.java
+++ src/test/java/io/jenkins/plugins/analysis/warnings/tasks/RemoteITest.java
@@ -1,17 +1,50 @@
 package io.jenkins.plugins.analysis.warnings.tasks;
 
+import java.util.List;
+
 import org.junit.Test;
 
+import org.jenkinsci.plugins.workflow.job.WorkflowJob;
+import hudson.model.Run;
 import hudson.model.Slave;
 
+import io.jenkins.plugins.analysis.core.model.ResultAction;
 import io.jenkins.plugins.analysis.core.testutil.IntegrationTestWithJenkinsPerSuite;
+
+import static edu.hm.hafner.analysis.assertj.Assertions.*;
 
 public class RemoteITest extends IntegrationTestWithJenkinsPerSuite {
 
     @Test
     public void defaultTest(){
+    //StepsITest, IntegrationTest
+
+        WorkflowJob job = createPipeline();
+
+        Slave slave1 = createAgent("mySlave1");
+        Slave slave2 = createAgent("mySlave2");
+
+        copySingleFileToAgentWorkspace(slave1, job, "../eclipse.txt", "issues.txt");
+        copySingleFileToAgentWorkspace(slave2, job, "../eclipse.txt", "issues.txt");
 
 
-        Slave slave = createAgent("mySlave");
+        Run<?, ?> run = buildSuccessfully(job);
+        List<ResultAction> actions = run.getActions(ResultAction.class);
+
+        assertThat(actions).hasSize(2);
+
+        ResultAction first;
+        ResultAction second;
+        if (actions.get(0).getId().equals("java-1")) {
+            first = actions.get(0);
+            second = actions.get(1);
+        }
+        else {
+            first = actions.get(1);
+            second = actions.get(0);
+        }
+
+        assertThat(first.getResult().getIssues()).hasSize(5);
+        assertThat(second.getResult().getIssues()).hasSize(3);
     }
 }
